560b9ef14d9162438c94884555a775f8c5b4c557
KevonneM <Kevonnem@gmail.com>
2021-11-07 12:41:53 -0700

    Added 404 for invalid topic id

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+
 # Create your views here.
 
 def index(request):
@@ -19,7 +20,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Shows single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # Make sure the topic belongs to the current user.
     if topic.owner != request.user:
         raise Http404
