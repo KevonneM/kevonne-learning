8bbc5b155b3758cb3cbd790f57443e26942a398a
KevonneM <Kevonnem@gmail.com>
2021-11-07 12:19:02 -0700

    Set DEBUG based on environment variables.

--- project_structure/settings.py
+++ project_structure/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -137,3 +137,8 @@ LOGIN_URL = 'users:login'
 # Heroku settings.
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
